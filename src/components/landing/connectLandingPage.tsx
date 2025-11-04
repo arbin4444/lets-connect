a8f45e477a5249bcd0eb1b43cd47ab085c1b033c
arbin4444 <arbinchaudhary@gmail.com>
2025-11-04 16:56:08 +0545

    working on landing page

--- src/components/landing/connectLandingPage.tsx
+++ src/components/landing/connectLandingPage.tsx
@@ -1,20 +1,47 @@
 import React from "react";
+import suit1 from "../../assets/clothes/suit1.jpg";
 
 export const ConnectLandingPage: React.FC = () => {
   return (
     <>
       <div className="flex">
         <div className="w-1/2">
-          <div className="flex justify-around shadow-[2px_2px_2px_0_rgba(0,0,0,0.25)]">
-            <span>Home</span>
-            <span>About us</span>
-            <span>Activity</span>
+          <div className="flex justify-around h-[50px] items-center">
+            <span className="text-[22px] hover:transition-transform duration-200 hover:scale-[1.2]">
+              Home
+            </span>
+            <span className="text-[22px] hover:transition-transform duration-200 hover:scale-[1.2]">
+              About us
+            </span>
+            <span className="text-[22px] hover:transition-transform duration-200 hover:scale-[1.2]">
+              Activity
+            </span>
           </div>
         </div>
         <div className="w-1/2 flex justify-end">
-        <div className="flex w-1/2 justify-around">
-          <span>Sign In</span>
-          <span>Sign Up</span>
+          <div className="flex w-1/3 justify-around h-[50px] items-center">
+            <span className="text-[22px] hover:transition-transform duration-200 hover:scale-[1.2]">
+              Sign In
+            </span>
+            <span className="text-[22px] hover:transition-transform duration-200 hover:scale-[1.2]">
+              Sign Up
+            </span>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="mx-[250px]">
+          <div className="flex">
+            <div className="w-1/2">
+              <div className="flex flex-col h-full justify-center items-center text-[28px]">
+                <p>Join us - connect with style,</p>
+                <p>create with passion, and</p>
+                <p>celebrate yourself.</p>
+              </div>
+            </div>
+            <div className="w-1/2">
+              <img className="h-[400px]" src={suit1} alt="suit" />
+            </div>
           </div>
         </div>
       </div>
